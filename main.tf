2c2e8616c65a018f163a20010826800ebd43349e
Cloudlightcorp <242549818+Cloudlightcorp@users.noreply.github.com>
2025-11-11 17:03:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,14 @@
 ###########################################
-# Provider Configuration
+# Terraform Backend Configuration (S3)
 ###########################################
+
 terraform {
+  backend "s3" {
+    bucket = "terraform-project1-state-pavithra"   # <-- change to your bucket name
+    key    = "ec2-project/terraform.tfstate"       # file name inside S3 bucket
+    region = "us-west-2"                           # Oregon
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -10,17 +17,20 @@ terraform {
   }
 }
 
+###########################################
+# Provider Configuration
+###########################################
 provider "aws" {
-  region = "us-west-2" # Oregon
+  region = "us-west-2"
 }
 
 ###########################################
 # EC2 Instance (using existing key pair)
 ###########################################
 resource "aws_instance" "project1_ec2" {
-  ami           = "ami-063fd45469b67083e" # Amazon Linux 2 (us-west-2)
+  ami           = "ami-063fd45469b67083e"  # Amazon Linux 2 AMI (Oregon)
   instance_type = "t3.micro"
-  key_name      = "project_1"             # existing key pair name
+  key_name      = "project_1"              # existing key pair name
 
   tags = {
     Name = "project1-ec2"
