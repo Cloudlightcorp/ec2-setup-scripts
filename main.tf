a4a4b2ba3185f7139a51ad1dfbebd4a2ee23999c
Cloudlightcorp <242549818+Cloudlightcorp@users.noreply.github.com>
2025-11-11 16:14:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,28 @@
 ###########################################
-  2 # Provider Configuration
-  3 ###########################################
-  4 terraform {
-  5   required_providers {
-  6     aws = {
-  7       source  = "hashicorp/aws"
-  8       version = "~> 5.0"
-  9     }
- 10   }
- 11 }
- 12 
- 13 provider "aws" {
- 14   region = "us-west-2" # Oregon
- 15 }
- 16 
- 17 ###########################################
- 18 # EC2 Instance (using existing key pair)
- 19 ###########################################
- 20 resource "aws_instance" "project1_ec2" {
- 21   ami           = "ami-063fd45469b67083e" # Amazon Linux 2 (us-west-2)
- 22   instance_type = "t3.micro"
- 23   key_name      = "project_1"              # existing key pair name
- 24 
- 25   tags = {
- 26     Name = "project1-ec2"
- 27   }
- 28 }
+# Provider Configuration
+###########################################
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2" # Oregon
+}
+
+###########################################
+# EC2 Instance (using existing key pair)
+###########################################
+resource "aws_instance" "project1_ec2" {
+  ami           = "ami-063fd45469b67083e" # Amazon Linux 2 (us-west-2)
+  instance_type = "t3.micro"
+  key_name      = "project_1"             # existing key pair name
+
+  tags = {
+    Name = "project1-ec2"
+  }
+}
